53751eb3da56c6bc4e71d674ad33f6d219cbb128
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-03-04 15:58:37 +0100

    removed Sqlite

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
-import sqlite from 'sqlite';
 import dotenv from 'dotenv';
 
 import { resolvers, typeDefs } from './schema';
@@ -9,16 +8,11 @@ dotenv.config();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-sqlite.open('./db.sqlite').then(db => {
-  global.db = db;
-  new ApolloServer({
-    typeDefs,
-    resolvers,
-    playground: true,
-    context: ({ req }) =>
-      req.headers['x-forwarded-for'] ||
-      req.connection.remoteAddress ||
-      '8.8.8.8',
-  }).applyMiddleware({ app });
-  app.listen(PORT, () => `Listening at http://localhost:${PORT}`);
-});
+new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  context: ({ req }) =>
+    req.headers['x-forwarded-for'] || req.connection.remoteAddress || '8.8.8.8',
+}).applyMiddleware({ app });
+app.listen(PORT, () => `Listening at http://localhost:${PORT}`);
