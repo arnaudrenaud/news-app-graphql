f707188f0778448d45766e2fea48cfb2e3a3ffed
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-03-05 14:49:04 +0100

    Serve client app from Node.js server

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import Home from './components/Home';
 
 const App = () => (
   <div className="App">
+    <header>
+      <img src="/images/banner-958962_960_720.webp" alt="header-banner" />
+    </header>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import { resolvers, typeDefs } from './schema';
 
@@ -16,4 +17,9 @@ new ApolloServer({
     ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
   }),
 }).applyMiddleware({ app });
+const BUILD_PATH = path.join(__dirname, '..', 'client', 'build');
+app.use(express.static(BUILD_PATH));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(BUILD_PATH, 'index.html'));
+});
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
